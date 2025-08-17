d0b1114593ae73e02b40674da0bad746376621f2
Arya-chaurasia <aryachaurasia11@gmail.com>
2025-08-17 21:03:55 +0530

    env added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" :  "/api";
+//This is for AWS ec2 deployment
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" :  "/api";
 
 
- //export const BASE_URL =  "/api"
+// For Create React App, use process.env; for Vite, use import.meta.env
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
